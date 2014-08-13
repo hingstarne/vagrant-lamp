036df41fcf839d2c10bde87414a1013b9c5ebd21
HIngst, Arne-Kristian <arne@hingst.it>
2014-08-13 23:29:05 +0200

    Missing :

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,7 @@ file { '/etc/apt/sources.list.d': ensure => directory;}->
 apt::ppa { 'ppa:ondrej/php5-oldstable': }->
 class{'php':}->
 class { ['php::fpm', 'php::cli', 'php::extension::apc']:}->
-package{'php5-mysql'}->
+package{'php5-mysql':}->
 class {'mysql::server':}
 class {'mongodb::server':
       noprealloc => true;
