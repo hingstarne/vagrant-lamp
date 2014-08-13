fb9491f89e946a33016c8659278dfef59b6ceb25
HIngst, Arne-Kristian <arne@hingst.it>
2014-08-13 23:24:33 +0200

    Added puppet module for mysql and for mongodb

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,9 @@
 file { '/etc/apt/sources.list.d': ensure => directory;}->
 apt::ppa { 'ppa:ondrej/php5-oldstable': }->
 class{'php':}->
-class { ['php::fpm', 'php::cli', 'php::extension::apc']:
-
-}
+class { ['php::fpm', 'php::cli', 'php::extension::apc']:}->
+package{'php5-mysql'}->
+class {'mysql::server':}
+class {'mongodb::server':
+      noprealloc => true;
+      }
